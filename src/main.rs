5a95e8a090657a97c78479264373c36ae028cd67
dys <1115410174@qq.com>
2023-11-27 20:25:50 +0800

    arc share the memory

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
+use std::sync::Arc;
 use std::thread;
 fn main() {
-    let v = vec![1, 2, 3, 4];
-
-    thread::scope(|s| {
-        s.spawn(|| {
-            println!(
-                "{:?} computes the sum of v is {}",
-                thread::current().id(),
-                v.iter().sum::<i32>()
-            );
-        });
-        s.spawn(|| {
-            println!(
-                "{:?} computes the product of v is {}",
-                thread::current().id(),
-                v.iter().product::<i32>()
-            )
-        });
+    let v = Arc::new(vec![1, 2, 3, 4]);
+    let v1 = v.clone();
+    let v2 = v.clone();
+    let t1 = thread::spawn(move || {
+        println!("{}", v1.as_ref().len());
     });
+    let t2 = thread::spawn(move || {
+        for v in v2.as_ref() {
+            println!("{:?}", v);
+        }
+    });
+    t1.join().unwrap();
+    t2.join().unwrap();
     println!("the main thread {:?} stops", thread::current().id());
 }
