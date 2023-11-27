34034a0b6f54b5b4c8857d0b4cfc2274320cad92
dys <1115410174@qq.com>
2023-11-27 19:48:21 +0800

    basic example 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 use std::thread;
 fn main() {
-    let t1 = thread::spawn(f);
-    t1.join().unwrap();
-    let t2 = thread::spawn(f);
+    let v = vec![1, 2, 3];
 
-    println!("Hello from the main thread. {:?}", thread::current().id());
-
-    t2.join().unwrap();
-}
-
-fn f() {
-    println!("Hello from another thread");
-
-    let id = thread::current().id();
-
-    println!("This is my thread id: {id:?}");
+    let _ = thread::spawn(move || {
+        for num in v {
+            println!("{}", num);
+        }
+    })
+    .join()
+    .unwrap();
 }
