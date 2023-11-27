4daf85273a6bb3b6e608898cf725c8b6b18fbff3
dys <1115410174@qq.com>
2023-11-27 19:44:19 +0800

    basic example 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::thread;
 fn main() {
-    println!("hello world");
+    let t1 = thread::spawn(f);
+    t1.join().unwrap();
+    let t2 = thread::spawn(f);
+
+    println!("Hello from the main thread. {:?}", thread::current().id());
+
+    t2.join().unwrap();
+}
+
+fn f() {
+    println!("Hello from another thread");
+
+    let id = thread::current().id();
+
+    println!("This is my thread id: {id:?}");
 }
